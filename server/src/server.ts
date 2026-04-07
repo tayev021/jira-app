6b3504aa5a1062dd426b0d7db6ec8b5a041d6ead
Eugene Taranov <tayev021@gmail.com>
2026-04-07 11:25:51 +0300

    feat(server): add database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,15 @@
+import mongoose from 'mongoose';
 import { app } from './app';
 
+const MONGO_URI = process.env.MONGO_URI as string;
 const PORT = process.env.SERVER_PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log('MongoDB CONNECTED!'))
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
+  })
+  .catch((error) => {
+    console.error('MongoDB connection ERROR!:', error);
+  });
