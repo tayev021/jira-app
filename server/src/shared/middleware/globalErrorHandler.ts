e3d804070341abaee855cfcc53de7ba4a4b0209f
Eugene Taranov <tayev021@gmail.com>
2026-04-08 21:58:59 +0300

    feat(server): add shared globalErrorHandler middleware

---
+++ server/src/shared/middleware/globalErrorHandler.ts
@@ -0,0 +1,22 @@
+import { AppError } from '../errors';
+import { NextFunction, Request, Response } from 'express';
+
+export async function globalErrorHandler(
+  error: AppError | Error,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  if (error instanceof AppError) {
+    res.status(error.status).json({
+      code: error.code,
+      message: error.message,
+      details: error.details || null,
+    });
+  } else {
+    res.status(500).json({
+      code: 'INTERNAL',
+      message: 'Something went wrong',
+    });
+  }
+}
