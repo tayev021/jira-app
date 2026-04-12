4398f293e41e2eba06978cf1642f176c5f983dee
Eugene Taranov <tayev021@gmail.com>
2026-04-12 19:40:16 +0300

    feat(server): add mapUser util

---
+++ server/src/shared/utils/mapUser.ts
@@ -0,0 +1,14 @@
+import { User } from '../../modules/user/user.model';
+import { CurrentUser } from '../types/CurrentUser';
+
+export function mapUser(user: User): CurrentUser {
+  return {
+    id: user._id.toString(),
+    name: user.name,
+    surname: user.surname,
+    email: user.email,
+    avatar: user.avatar,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
+}
