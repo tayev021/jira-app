36cb8a483309a1f469719362928901531844c49f
Eugene Taranov <tayev021@gmail.com>
2026-04-12 19:37:52 +0300

    fix(server): fix ValidationError

--- server/src/shared/errors/ValidationError.ts
+++ server/src/shared/errors/ValidationError.ts
@@ -1,5 +1,5 @@
 import { AppError } from './AppError';
-import { FieldValidationError } from 'shared/types/FieldValidationError';
+import { FieldValidationError } from '../types/FieldValidationError';
 
 export class ValidationError extends AppError {
   constructor(message: string, fields: FieldValidationError[]) {
