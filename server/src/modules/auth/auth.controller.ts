287838a95ac02850ba5a4e48564ae26ec7d00b30
Eugene Taranov <tayev021@gmail.com>
2026-04-12 19:46:04 +0300

    feat(server): add AuthController to auth module

---
+++ server/src/modules/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { authService } from './auth.service';
+import { catchAsync } from '../../shared/utils/catchAsync';
+import { getCookieOptions } from '../../shared/utils/getCookieOptions';
+
+class AuthController {
+  me = (req: Request, res: Response) => {
+    res.status(200).json({ user: req.currentUser });
+  };
+
+  signUp = catchAsync(async (req: Request, res: Response) => {
+    const { name, surname, email, password } = req.body;
+    const { user, token } = await authService.signUp({
+      name,
+      surname,
+      email,
+      password,
+    });
+
+    res.cookie('token', token, getCookieOptions());
+    res.status(201).json({ user });
+  });
+
+  signIn = catchAsync(async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const { user, token } = await authService.signIn({ email, password });
+
+    res.cookie('token', token, getCookieOptions());
+    res.status(200).json({ user });
+  });
+
+  signOut = (req: Request, res: Response) => {
+    res.clearCookie('token', getCookieOptions());
+    res.status(200).json({});
+  };
+}
+
+export const authController = new AuthController();
