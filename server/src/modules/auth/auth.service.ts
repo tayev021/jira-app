8c0546aecb38b8c1f10a1c6610c13f0e9494585e
Eugene Taranov <tayev021@gmail.com>
2026-04-12 19:45:30 +0300

    feat(server): add AuthService to auth module

---
+++ server/src/modules/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { userService } from '../user/user.service';
+import { AppError, UnauthorizedError } from '../../shared/errors';
+import bcrypt from 'bcryptjs';
+import { signToken } from '../../shared/utils/signToken';
+import { mapUser } from '../../shared/utils/mapUser';
+
+class AuthService {
+  signUp = async (userData: {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+  }) => {
+    const existingUser = await userService.getUserByEmail(userData.email);
+
+    if (existingUser) {
+      throw new AppError(400, 'ERROR', `A user with this email already exists`);
+    }
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const user = await userService.createUser({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    const token = signToken({ id: user._id.toString() });
+
+    return { user: mapUser(user), token };
+  };
+
+  signIn = async (userData: { email: string; password: string }) => {
+    const user = await userService.getUserByEmail(userData.email);
+
+    if (!user) {
+      throw new UnauthorizedError('Invalid email or password');
+    }
+
+    const isMatch = await bcrypt.compare(userData.password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedError('Invalid email or password');
+    }
+
+    const token = signToken({ id: user._id.toString() });
+
+    return { user: mapUser(user), token };
+  };
+}
+
+export const authService = new AuthService();
