5d2b47bb985af0e6c686ab1613a55946a52f0931
Eugene Taranov <tayev021@gmail.com>
2026-04-12 19:37:20 +0300

    feat(server): update UnauthorizedError

--- server/src/shared/errors/UnauthorizedError.ts
+++ server/src/shared/errors/UnauthorizedError.ts
@@ -1,11 +1,9 @@
 import { AppError } from './AppError';
 
 export class UnauthorizedError extends AppError {
-  constructor() {
-    super(
-      401,
-      'UNAUTHORIZED',
-      'You are not signed in! Please sign in to get access!'
-    );
+  constructor(
+    message = 'You are not signed in! Please sign in to get access!'
+  ) {
+    super(401, 'UNAUTHORIZED', message);
   }
 }
