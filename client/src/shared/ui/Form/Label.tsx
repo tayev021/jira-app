1bd0d279b7a0920e1a4a853026eed693de345aff
Eugene Taranov <tayev021@gmail.com>
2026-04-17 21:34:04 +0300

    feat(client): add shared Form.Label ui

---
+++ client/src/shared/ui/Form/Label.tsx
@@ -0,0 +1,29 @@
+import type { ComponentProps } from 'react';
+import { useFieldContext } from '../../hooks/useFieldContext';
+import { cn } from '../../utils/cn';
+
+type LabelProps = {
+  children: string;
+  className?: string;
+} & ComponentProps<'label'>;
+
+export function Label({ children, className = '', ...rest }: LabelProps) {
+  const { isFocused, hasValue, hasError } = useFieldContext();
+
+  return (
+    <label
+      className={cn(
+        'absolute left-3 -translate-y-1/2 px-2 py-0.5 rounded-xl font-medium transition-all cursor-text',
+        isFocused || hasValue
+          ? 'top-0 text-white bg-primary'
+          : 'top-1/2 bg-transparent',
+        !isFocused && hasValue ? 'bg-zinc-400' : '',
+        isFocused && hasError ? 'bg-red-500' : '',
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </label>
+  );
+}
