8ee3fc58de27cd5a17a98c8530e73e5ae282b18c
Eugene Taranov <tayev021@gmail.com>
2026-04-23 20:41:13 +0300

    feat(client): update AppRouter

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from '../layouts/MainLayout';
 import { HomePage } from '../../pages/home';
 import { SignInPage, SignUpPage } from '../../pages/auth';
 import { ProtectedRoute } from './ProtectedRoute';
+import { AppHomePage } from '../../pages/app';
 
 export function AppRouter() {
   return (
@@ -14,14 +15,12 @@ export function AppRouter() {
             <Route path="signin" element={<SignInPage />} />
             <Route path="signup" element={<SignUpPage />} />
           </Route>
-          <Route
-            path="account"
-            element={
-              <ProtectedRoute>
-                <div>Account Page</div>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="app" element={<ProtectedRoute />}>
+            <Route index element={<AppHomePage />} />
+          </Route>
+          <Route path="account" element={<ProtectedRoute />}>
+            <Route index element={<div>Account Page</div>} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
