c5c1e6d02b00013c6214d081550fda8a0eca98d1
Eugene Taranov <tayev021@gmail.com>
2026-04-23 20:35:11 +0300

    refactor(client): rewrite shared api

--- client/src/shared/api/api.ts
+++ client/src/shared/api/api.ts
@@ -1,23 +1,22 @@
 import { fetchBase } from './fetchBase';
 import { refresh } from './refresh';
+import { clearAccessToken } from './tokenStore';
 
 export async function api(
   url: string,
-  options: RequestInit = {},
-  retry = true
+  options: RequestInit & { auth?: boolean } = {}
 ) {
-  let response = await fetchBase(url, options);
+  const { auth = false, ...restOptions } = options;
+  let response = await fetchBase(url, restOptions);
 
-  if (response.status === 401 && retry) {
-    const accessToken = await refresh();
-
-    response = await fetchBase(url, {
-      ...options,
-      headers: {
-        ...(options.headers || {}),
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+  if (response.status === 401 && auth) {
+    try {
+      await refresh();
+      response = await fetchBase(url, restOptions);
+    } catch (error) {
+      clearAccessToken();
+      throw error;
+    }
   }
 
   return response;
