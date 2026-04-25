a201815c9234b9c62f20698782fa4a6e23015686
Eugene Taranov <tayev021@gmail.com>
2026-04-25 20:54:41 +0300

    feat(server): update ForbiddenError

--- server/src/shared/errors/ForbiddenError.ts
+++ server/src/shared/errors/ForbiddenError.ts
@@ -1,11 +1,7 @@
 import { ApiError } from './ApiError';
 
 export class ForbiddenError extends ApiError {
-  constructor() {
-    super(
-      403,
-      'FORBIDDEN',
-      'You do not have permission to perform this action'
-    );
+  constructor(message = 'You do not have permission to perform this action') {
+    super(403, 'FORBIDDEN', message);
   }
 }
