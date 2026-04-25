1772ede7c1245cde3ed5ae32e49a4d1a0adac2f6
Eugene Taranov <tayev021@gmail.com>
2026-04-25 20:57:31 +0300

    feat(server): add workspace controller

---
+++ server/src/modules/workspace/workspace.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { catchAsync } from '../../shared/utils/catchAsync';
+import { workspaceService } from './workspace.service';
+
+class WorkspaceController {
+  getWorkspaces = catchAsync(async (req: Request, res: Response) => {
+    const { workspaces } = await workspaceService.getWorkspaces(
+      req.currentUser!.id
+    );
+    res.status(200).json({ success: true, data: { workspaces } });
+  });
+
+  getWorkspace = catchAsync(async (req: Request, res: Response) => {
+    const { workspace } = await workspaceService.getWorkspace({
+      workspaceId: req.params.workspaceId as string,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { workspace } });
+  });
+
+  createWorkspace = catchAsync(async (req: Request, res: Response) => {
+    const { workspace } = await workspaceService.createWorkspace({
+      name: req.body.name,
+      ownerId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { workspace } });
+  });
+
+  updateWorkspaceName = catchAsync(async (req: Request, res: Response) => {
+    const { workspace } = await workspaceService.updateWorkspaceName({
+      workspaceId: req.params.workspaceId as string,
+      name: req.body.name,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { workspace } });
+  });
+
+  deleteWorkspace = catchAsync(async (req: Request, res: Response) => {
+    await workspaceService.deleteWorkspace({
+      workspaceId: req.params.workspaceId as string,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: {} });
+  });
+}
+
+export const workspaceController = new WorkspaceController();
