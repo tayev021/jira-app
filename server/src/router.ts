2484f01e54774d47007f0dbec60d3e5c11faba97
Eugene Taranov <tayev021@gmail.com>
2026-04-25 20:58:49 +0300

    feat(server): update root router

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { authRouter } from './modules/auth/auth.router';
 import { notFoundHandler } from './shared/middleware/notFoundHandler';
+import { workspaceRouter } from './modules/workspace/workspace.router';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/workspaces', workspaceRouter);
 
 router.use(notFoundHandler);
 
