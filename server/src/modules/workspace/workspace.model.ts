db94500c0c938ab459d3cf972eb82025dba303ec
Eugene Taranov <tayev021@gmail.com>
2026-04-29 20:18:19 +0300

    feat(server): update Workspace model

--- server/src/modules/workspace/workspace.model.ts
+++ server/src/modules/workspace/workspace.model.ts
@@ -5,32 +5,39 @@ export interface Workspace {
   name: string;
   owner: Types.ObjectId;
   members: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const workspaceSchema = new Schema<Workspace>({
-  name: {
-    type: String,
-    required: [true, 'A workspace must have a name'],
-    set(value: string) {
-      if (!value) return value;
-      return value
-        .split(' ')
-        .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
+const workspaceSchema = new Schema<Workspace>(
+  {
+    name: {
+      type: String,
+      required: [true, 'A workspace must have a name'],
+      set(value: string) {
+        if (!value) return value;
+        return value
+          .split(' ')
+          .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+          .join(' ');
+      },
     },
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: [true, 'A workspace must have a owner'],
-  },
-  members: [
-    {
+    owner: {
       type: Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
+      required: [true, 'A workspace must have a owner'],
     },
-  ],
-});
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Workspace = model<Workspace>('Workspace', workspaceSchema);
