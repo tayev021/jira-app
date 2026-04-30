7698d0c2d8e5a40a0fc3a8f8d246241345082bd8
Eugene Taranov <tayev021@gmail.com>
2026-04-30 21:02:50 +0300

    feat(server): add restrictToMember util

---
+++ server/src/shared/utils/restrictToMember.ts
@@ -0,0 +1,12 @@
+import { Workspace } from '../../modules/workspace/workspace.model';
+import { ForbiddenError } from '../errors';
+
+export async function restrictToMember(workspace: Workspace, userId: string) {
+  const isMember = workspace.memberIds.some((id) => id.toString() === userId);
+
+  if (!isMember) {
+    throw new ForbiddenError(
+      'You are not a member of this workspace. You do not have permission to perform this action'
+    );
+  }
+}
