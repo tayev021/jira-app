37cb366b44f24f8034a7baee32378cbbea381034
Eugene Taranov <tayev021@gmail.com>
2026-05-01 17:01:53 +0300

    feat(server): add mapIssue util

---
+++ server/src/shared/utils/mapIssue.ts
@@ -0,0 +1,17 @@
+import { Issue as IssueModel } from '../../modules/issue/issue.model';
+import { Issue } from '../types/Issue';
+
+export function mapIssue(issue: IssueModel): Issue {
+  return {
+    id: issue._id.toString(),
+    title: issue.title,
+    description: issue.description,
+    workspaceId: issue.workspaceId.toString(),
+    reporterId: issue.reporterId.toString(),
+    assigneeIds: issue.assigneeIds.map((id) => id.toString()),
+    status: issue.status,
+    priority: issue.priority,
+    createdAt: issue.createdAt.toISOString(),
+    updatedAt: issue.updatedAt.toISOString(),
+  };
+}
