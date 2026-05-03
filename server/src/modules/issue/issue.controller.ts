dc75373daabb4350328a4db1fcd6994d6350848e
Eugene Taranov <tayev021@gmail.com>
2026-05-03 20:32:47 +0300

    feat(server): add IssueController

---
+++ server/src/modules/issue/issue.controller.ts
@@ -0,0 +1,95 @@
+import { Request, Response } from 'express';
+import { catchAsync } from '../../shared/utils/catchAsync';
+import { issueService } from './issue.service';
+
+class IssueController {
+  getIssues = catchAsync(async (req: Request, res: Response) => {
+    const { issues } = await issueService.getIssues({
+      workspaceId: req.body.workspaceId,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issues } });
+  });
+
+  getIssue = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.getIssue({
+      issueId: req.params.issueId as string,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  create = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.create({
+      title: req.body.title,
+      description: req.body.description,
+      workspaceId: req.body.workspaceId,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  updateTitle = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.updateTitle({
+      issueId: req.params.issueId as string,
+      title: req.body.title,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  updateDescription = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.updateDescription({
+      issueId: req.params.issueId as string,
+      description: req.body.description,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  updateStatus = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.updateStatus({
+      issueId: req.params.issueId as string,
+      status: req.body.status,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  updatePriority = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.updatePriority({
+      issueId: req.params.issueId as string,
+      priority: req.body.priority,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  addAssignee = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.addAssignee({
+      issueId: req.params.issueId as string,
+      assigneeId: req.body.assigneeId,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  deleteAssignee = catchAsync(async (req: Request, res: Response) => {
+    const { issue } = await issueService.deleteAssignee({
+      issueId: req.params.issueId as string,
+      assigneeId: req.body.assigneeId,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: { issue } });
+  });
+
+  deleteIssue = catchAsync(async (req: Request, res: Response) => {
+    await issueService.deleteIssue({
+      issueId: req.params.issueId as string,
+      currentUserId: req.currentUser!.id,
+    });
+    res.status(200).json({ success: true, data: {} });
+  });
+}
+
+export const issueController = new IssueController();
