e2716d5b5ebb32deb1be07f1cae5ee5e0ab39e16
Eugene Taranov <tayev021@gmail.com>
2026-05-03 20:33:49 +0300

    feat(server): add IssueRouter

---
+++ server/src/modules/issue/issue.router.ts
@@ -0,0 +1,60 @@
+import { Router } from 'express';
+import { protect } from '../../shared/middleware/protect';
+import { issueController } from './issue.controller';
+import { validateBody } from '../../shared/middleware/validateBody';
+import { issueValidations } from './issue.validation';
+
+const issueRouter = Router();
+
+issueRouter.get(
+  '/',
+  protect,
+  validateBody(issueValidations.getIssuesSchema),
+  issueController.getIssues
+);
+issueRouter.get('/:issueId', protect, issueController.getIssue);
+issueRouter.post(
+  '/',
+  protect,
+  validateBody(issueValidations.createIssueSchema),
+  issueController.create
+);
+issueRouter.patch(
+  '/:issueId/title',
+  protect,
+  validateBody(issueValidations.updateTitleSchema),
+  issueController.updateTitle
+);
+issueRouter.patch(
+  '/:issueId/description',
+  protect,
+  validateBody(issueValidations.updateDescriptionSchema),
+  issueController.updateDescription
+);
+issueRouter.patch(
+  '/:issueId/status',
+  protect,
+  validateBody(issueValidations.updateStatusSchema),
+  issueController.updateStatus
+);
+issueRouter.patch(
+  '/:issueId/priority',
+  protect,
+  validateBody(issueValidations.updatePrioritySchema),
+  issueController.updatePriority
+);
+issueRouter.put(
+  '/:issueId/assignee',
+  protect,
+  validateBody(issueValidations.addAssigneeSchema),
+  issueController.addAssignee
+);
+issueRouter.delete(
+  '/:issueId/assignee',
+  protect,
+  validateBody(issueValidations.deleteAssigneeSchema),
+  issueController.deleteAssignee
+);
+issueRouter.delete('/:issueId', protect, issueController.deleteIssue);
+
+export { issueRouter };
