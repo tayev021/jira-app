44cec24885b32b3fab1363b0d2617c06f97659d4
Eugene Taranov <tayev021@gmail.com>
2026-05-08 15:48:14 +0300

    feat(client): update app router

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -3,7 +3,8 @@ import { MainLayout } from '../layouts/MainLayout';
 import { HomePage } from '../../pages/home';
 import { SignInPage, SignUpPage } from '../../pages/auth';
 import { ProtectedRoute } from './ProtectedRoute';
-import { AppHomePage } from '../../pages/app';
+import { AppHomePage, IssuesPage, SummaryPage } from '../../pages/app';
+import { WorkspaceLayout } from '../layouts/WorkspaceLayout';
 
 export function AppRouter() {
   return (
@@ -17,10 +18,18 @@ export function AppRouter() {
           </Route>
           <Route path="app" element={<ProtectedRoute />}>
             <Route index element={<AppHomePage />} />
+            <Route path="workspace/:workspaceId" element={<WorkspaceLayout />}>
+              <Route index element={<SummaryPage />} />
+              <Route path="issues" element={<IssuesPage />} />
+            </Route>
           </Route>
           <Route path="account" element={<ProtectedRoute />}>
             <Route index element={<div>Account Page</div>} />
           </Route>
+          <Route
+            path="user/:userId/profile"
+            element={<div>User Profile Page</div>}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
