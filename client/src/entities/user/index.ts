fc086eb7c74351a89f898982cfbb1215aecfffb1
Eugene Taranov <tayev021@gmail.com>
2026-05-08 15:36:07 +0300

    feat(client): update user entity

--- client/src/entities/user/index.ts
+++ client/src/entities/user/index.ts
@@ -1,4 +1,5 @@
 import { useMe } from './hooks/useMe';
 import { UserAvatar } from './ui/UserAvatar';
+import { UserLink } from './ui/UserLink';
 
-export { useMe, UserAvatar };
+export { useMe, UserAvatar, UserLink };
--- client/src/entities/user/ui/UserAvatar.tsx
+++ client/src/entities/user/ui/UserAvatar.tsx
@@ -1,14 +1,21 @@
 import type { User } from '../../../shared/types/User';
+import { cn } from '../../../shared/utils/cn';
 
 interface UserAvatarProps {
-  user: User;
+  user: Pick<User, 'name' | 'surname' | 'avatar'>;
+  className?: string;
 }
 
-export function UserAvatar({ user }: UserAvatarProps) {
+export function UserAvatar({ user, className = '' }: UserAvatarProps) {
   const initials = user.name[0] + user.surname[0];
 
   return (
-    <div className="w-6 h-6 flex justify-center items-center rounded-full text-xs font-semibold text-secondary-text bg-primary hover:bg-primary-dark">
+    <div
+      className={cn(
+        'w-6 h-6 flex justify-center items-center rounded-full text-xs font-semibold text-secondary-text bg-primary hover:bg-primary-dark',
+        className
+      )}
+    >
       {initials}
     </div>
   );
