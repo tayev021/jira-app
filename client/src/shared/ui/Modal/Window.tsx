eaea03a5dc9b8a54dde36411937f683b5dd0a2fc
Eugene Taranov <tayev021@gmail.com>
2026-05-10 20:47:13 +0300

    feat(client): add shared Modal ui

---
+++ client/src/shared/ui/Modal/Window.tsx
@@ -0,0 +1,36 @@
+import { createPortal } from 'react-dom';
+import { useModal } from '../../hooks/useModal';
+import { useOutsideClick } from '../../hooks/useOutsideClick';
+import { cloneElement, type ReactElement } from 'react';
+import { cn } from '../../utils/cn';
+
+interface WindowProps {
+  name: string;
+  children: ReactElement;
+  className?: string;
+}
+
+interface ChildElementProps {
+  close: () => void;
+}
+
+export function Window({ children, name, className = '' }: WindowProps) {
+  const { modalName, close } = useModal();
+  const ref = useOutsideClick<HTMLDivElement>(close);
+
+  if (name !== modalName) return null;
+
+  return createPortal(
+    <div className="fixed top-0 left-0 w-screen h-screen backdrop-blur-[2px] transition-all z-50">
+      <div
+        ref={ref}
+        className={cn('fixed top-1/2 left-1/2 -translate-1/2', className)}
+      >
+        {cloneElement(children as React.ReactElement<ChildElementProps>, {
+          close,
+        })}
+      </div>
+    </div>,
+    document.body
+  );
+}
