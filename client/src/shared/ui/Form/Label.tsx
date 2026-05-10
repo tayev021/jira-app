ee8d3f02a8ea75737e3fd4bbf6b85e9228297ea0
Eugene Taranov <tayev021@gmail.com>
2026-05-10 20:45:23 +0300

    feat(client): update shared Form.Label ui

--- client/src/shared/ui/Form/Label.tsx
+++ client/src/shared/ui/Form/Label.tsx
@@ -13,10 +13,10 @@ export function Label({ children, className = '', ...rest }: LabelProps) {
   return (
     <label
       className={cn(
-        'absolute left-3 -translate-y-1/2 px-2 py-0.5 rounded-xl font-medium transition-all cursor-text',
+        'absolute left-3 -translate-y-2 px-2 py-0.5 rounded-xl font-medium transition-all cursor-text',
         isFocused || hasValue
           ? 'top-0 text-secondary-text bg-gray-primary'
-          : 'top-1/2',
+          : 'top-5',
         isFocused ? 'bg-primary' : '',
         (isFocused || hasValue) && hasError ? 'bg-red-primary' : '',
         className
