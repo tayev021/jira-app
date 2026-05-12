5f1bcbf75c513fbe72416990dee7521a514bcf60
Eugene Taranov <tayev021@gmail.com>
2026-05-12 09:37:06 +0300

    feat(client): update AssigneesCell ui

--- client/src/widgets/Issues/ui/Table/cells/AssigneesCell.tsx
+++ client/src/widgets/Issues/ui/Table/cells/AssigneesCell.tsx
@@ -1,5 +1,4 @@
-import { HiOutlineUser } from 'react-icons/hi2';
-import { UserLink } from '../../../../../entities/user';
+import { NoUserAvatar, UserLink } from '../../../../../entities/user';
 import type { IssueUser } from '../../../../../shared/types/IssueUser';
 import { Cell } from './Cell';
 
@@ -11,13 +10,7 @@ export function AssigneesCell({ assignees }: AssigneesCellProps) {
   if (assignees.length <= 0) {
     return (
       <Cell>
-        <span
-          className={
-            'w-6 h-6 relative inline-block mr-2 rounded-full text-base align-middle text-primary-dark bg-gray-primary-light'
-          }
-        >
-          <HiOutlineUser className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
-        </span>
+        <NoUserAvatar className="mr-2" />
         <span className="align-middle">Unassigned</span>
       </Cell>
     );
