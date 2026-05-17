5d6be87927bdebdbf1592a6d8eadc8e1bbbae24e
Eugene Taranov <tayev021@gmail.com>
2026-05-17 20:26:35 +0300

    fix(client): fix IssueDetails.Actions bug

--- client/src/widgets/IssueDetails/ui/Actions.tsx
+++ client/src/widgets/IssueDetails/ui/Actions.tsx
@@ -19,12 +19,12 @@ export function Actions({ issue }: ActionsProps) {
   return (
     <ul className="mt-8 mx-auto">
       <li>
-        <Modal.Open modalName={`delete-issue-${issue.id}`}>
+        <Modal.Open modalName={`delete-issue-${issue.id}-from-issue-details`}>
           <Button className="px-4 py-1 border border-gray-primary rounded-md text-base text-gray-primary leading-none hover:border-red-primary hover:text-red-primary hover:shadow-md">
             Delete Issue
           </Button>
         </Modal.Open>
-        <Modal.Window name={`delete-issue-${issue.id}`}>
+        <Modal.Window name={`delete-issue-${issue.id}-from-issue-details`}>
           <DeleteIssue issue={issue} handleSuccess={handleSuccessDelete} />
         </Modal.Window>
       </li>
