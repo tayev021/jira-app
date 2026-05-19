e281f28a08492f69fc201ff3fae79456e4b7483c
Eugene Taranov <tayev021@gmail.com>
2026-05-19 21:03:37 +0300

    feat(server): update mapWorkspace util

--- server/src/shared/utils/mapWorkspace.ts
+++ server/src/shared/utils/mapWorkspace.ts
@@ -1,13 +1,14 @@
-import { Workspace as WorkspaceModel } from '../../modules/workspace/workspace.model';
+import { PopulatedWorkspace } from '../../modules/workspace/workspace.model';
 import { Workspace } from '../types/Workspace';
+import { mapUser } from './mapUser';
 
-export function mapWorkspace(workspace: WorkspaceModel): Workspace {
+export function mapWorkspace(workspace: PopulatedWorkspace): Workspace {
   return {
     id: workspace._id.toString(),
     slug: workspace.slug,
     name: workspace.name,
-    ownerId: workspace.ownerId.toString(),
-    memberIds: workspace.memberIds.map((id) => id.toString()),
+    owner: mapUser(workspace.ownerId),
+    members: workspace.memberIds.map((member) => mapUser(member)),
     createdAt: workspace.createdAt.toISOString(),
     updatedAt: workspace.updatedAt.toISOString(),
   };
