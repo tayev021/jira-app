aff0a8652825f31deee83e9d2ffd58bc6b3d831c
Eugene Taranov <tayev021@gmail.com>
2026-05-22 11:35:40 +0300

    feat(server): update UserService

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -1,22 +1,41 @@
+import { ApiError, ForbiddenError } from '../../shared/errors';
 import { mapUser } from '../../shared/utils/mapUser';
+import { Issue } from '../issue/issue.model';
+import { Workspace } from '../workspace/workspace.model';
 import { User } from './user.model';
 
 class UserService {
-  createUser = async (userData: {
+  getUsers = async () => {
+    const users = await User.find();
+    return { users: users.map(mapUser) };
+  };
+
+  createUser = async (data: {
     name: string;
     surname: string;
     email: string;
     password: string;
   }) => {
-    return User.create(userData);
+    return User.create(data);
   };
 
   getUserByEmail = async (email: string) => {
     return User.findOne({ email }).select('+password');
   };
 
-  searchUsers = async (data: { query: string }) => {
+  searchUsers = async (data: { query: string; workspaceId: string }) => {
+    const workspace = await Workspace.findById(data.workspaceId).select(
+      'memberIds'
+    );
+
+    if (!workspace) {
+      throw new ApiError(400, 'ERROR', 'Workspace with this ID does not exist');
+    }
+
     const users = await User.find({
+      _id: {
+        $nin: workspace.memberIds,
+      },
       $or: [
         { name: { $regex: data.query, $options: 'i' } },
         { surname: { $regex: data.query, $options: 'i' } },
@@ -24,6 +43,47 @@ class UserService {
     }).limit(10);
 
     return { users: users.map(mapUser) };
+  };
+
+  deleteAccount = async (data: { userId: string }) => {
+    const { userId } = data;
+
+    const ownedWorkspaces = await Workspace.find({ ownerId: userId });
+
+    if (ownedWorkspaces.length > 0) {
+      throw new ForbiddenError(
+        `You have active workspaces. You cannot delete your account`
+      );
+    }
+
+    const reportedIssues = await Issue.find({ reporterId: userId });
+
+    if (reportedIssues.length > 0) {
+      throw new ForbiddenError(
+        `You have reported issues. You cannot delete your account`
+      );
+    }
+
+    await Promise.all([
+      Workspace.updateMany(
+        { memberIds: userId },
+        {
+          $pull: {
+            memberIds: userId,
+          },
+        }
+      ),
+      Issue.updateMany(
+        { assigneeIds: userId },
+        {
+          $pull: {
+            assigneeIds: userId,
+          },
+        }
+      ),
+    ]);
+
+    await User.findByIdAndDelete(userId);
   };
 }
 
