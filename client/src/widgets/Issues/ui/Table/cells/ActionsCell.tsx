3cc1700885b99f1d7019d09174aa62141707fc92
Eugene Taranov <tayev021@gmail.com>
2026-05-25 21:01:53 +0300

    feat(client): update Issues widget

--- client/src/widgets/Issues/ui/Table/cells/ActionsCell.tsx
+++ client/src/widgets/Issues/ui/Table/cells/ActionsCell.tsx
@@ -1,4 +1,6 @@
 import type { Issue } from '../../../../../shared/types/Issue';
+import { useAuth } from '../../../../../shared/hooks/useAuth';
+import { useWorkspace } from '../../../../../entities/workspace';
 import { Cell } from './Cell';
 import { Modal } from '../../../../../shared/ui/Modal';
 import { Button } from '../../../../../shared/ui/Button';
@@ -10,10 +12,19 @@ interface ActionsCellProps {
 }
 
 export function ActionsCell({ issue }: ActionsCellProps) {
+  const { currentUser } = useAuth();
+  const { workspace } = useWorkspace();
+
   return (
     <Cell>
       <Modal.Open modalName={`delete-issue-${issue.id}`}>
-        <Button className="flex items-center mx-auto text-lg text-gray-primary hover:text-red-primary">
+        <Button
+          className="flex items-center mx-auto text-lg text-gray-primary hover:text-red-primary disabled:cursor-not-allowed"
+          disabled={
+            currentUser?.id !== issue.reporter.id &&
+            currentUser?.id !== workspace?.owner.id
+          }
+        >
           <HiOutlineTrash />
         </Button>
       </Modal.Open>
