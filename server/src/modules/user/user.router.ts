134190df1b8f834dfcc0d9ee5adb14b98f8d7e62
Eugene Taranov <tayev021@gmail.com>
2026-05-25 20:57:35 +0300

    feat(server): update UserRouter

--- server/src/modules/user/user.router.ts
+++ server/src/modules/user/user.router.ts
@@ -3,6 +3,8 @@ import { protect } from '../../shared/middleware/protect';
 import { userController } from './user.controller';
 import { validateQueryParams } from '../../shared/middleware/validateQueryParams';
 import { userValidation } from './user.validation';
+import { uploadAvatar } from '../../shared/middleware/upload';
+import { resizeAvatar } from '../../shared/middleware/resizeAvatar';
 
 const userRouter = Router();
 
@@ -12,6 +14,13 @@ userRouter.get(
   protect,
   validateQueryParams(userValidation.searchUsers),
   userController.searchUsers
+);
+userRouter.patch(
+  '/avatar',
+  protect,
+  uploadAvatar,
+  resizeAvatar,
+  userController.updateAvatar
 );
 userRouter.delete('/', protect, userController.deleteAccount);
 
