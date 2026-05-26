cfe747c90abf03e6f0d342d213d628581cad825f
Eugene Taranov <tayev021@gmail.com>
2026-05-26 09:35:33 +0300

    feat(client): update shared Container ui

--- client/src/shared/ui/Container.tsx
+++ client/src/shared/ui/Container.tsx
@@ -2,12 +2,16 @@ import type { ReactNode } from 'react';
 import { cn } from '../utils/cn';
 
 interface ContainerProps {
-  children: ReactNode;
+  children: ReactNode | ReactNode[];
   className?: string;
 }
 
 export function Container({ children, className = '' }: ContainerProps) {
   return (
-    <div className={cn('max-w-300 mx-auto p-5', className)}>{children}</div>
+    <div
+      className={cn('max-w-300 min-h-0 mx-auto p-5 overflow-hidden', className)}
+    >
+      {children}
+    </div>
   );
 }
