6d599cb447c53698f441d23f5f29b213be7de407
Eugene Taranov <tayev021@gmail.com>
2026-05-26 09:40:54 +0300

    feat(client): update app page import

--- client/src/pages/app/index.ts
+++ client/src/pages/app/index.ts
@@ -1,4 +1,15 @@
 import { AppHomePage } from './home/AppHomePage';
-import { BoardPage, IssuesPage, SummaryPage } from './workspace';
+import { SummaryPage } from './workspace/summary/SummaryPage';
+import { MembersPage } from './workspace/members/MembersPage';
+import { IssuesPage } from './workspace/issues/IssuesPage';
+import { BoardPage } from './workspace/board/BoardPage';
+import { SettingsPage } from './workspace/settings/SettingsPage';
 
-export { AppHomePage, SummaryPage, IssuesPage, BoardPage };
+export {
+  AppHomePage,
+  SummaryPage,
+  MembersPage,
+  IssuesPage,
+  BoardPage,
+  SettingsPage,
+};
