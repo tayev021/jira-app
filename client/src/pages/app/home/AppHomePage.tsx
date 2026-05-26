2108a0c5a2293391e3a6158ccdb6db2f1e4f62f6
Eugene Taranov <tayev021@gmail.com>
2026-05-26 09:38:32 +0300

    feat(client): update app/home page ui

--- client/src/pages/app/home/AppHomePage.tsx
+++ client/src/pages/app/home/AppHomePage.tsx
@@ -9,18 +9,16 @@ export function AppHomePage() {
   if (!currentUser) return null;
 
   return (
-    <div>
-      <Container>
-        <div className="p-5 mb-5 rounded-lg bg-linear-[150deg] from-primary from-40% to-blue-primary to-70%  text-text-secondary">
-          <h4 className="mb-2 text-lg font-semibold text-secondary-text">
-            {formatDate(new Date(), 'WWWW, MMM DD')}
-          </h4>
-          <h3 className="text-xl font-bold text-secondary-text">
-            Hello, {currentUser.name} {currentUser.surname}
-          </h3>
-        </div>
-        <Workspaces />
-      </Container>
-    </div>
+    <Container>
+      <div className="p-5 mb-5 rounded-lg bg-linear-[150deg] from-primary from-40% to-blue-primary to-70%  text-text-secondary">
+        <h4 className="mb-2 text-lg font-semibold text-secondary-text">
+          {formatDate(new Date(), 'WWWW, MMM DD')}
+        </h4>
+        <h3 className="text-xl font-bold text-secondary-text">
+          Hello, {currentUser.name} {currentUser.surname}
+        </h3>
+      </div>
+      <Workspaces />
+    </Container>
   );
 }
