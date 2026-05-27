1049ea26844394412793d985c3edb3ac3598a924
Eugene Taranov <tayev021@gmail.com>
2026-05-27 22:02:29 +0300

    feat(client): update shared Container ui

--- client/src/shared/ui/Container.tsx
+++ client/src/shared/ui/Container.tsx
@@ -9,7 +9,10 @@ interface ContainerProps {
 export function Container({ children, className = '' }: ContainerProps) {
   return (
     <div
-      className={cn('max-w-300 min-h-0 mx-auto p-5 overflow-hidden', className)}
+      className={cn(
+        'max-w-300 h-full flex flex-col mx-auto p-5 overflow-hidden',
+        className
+      )}
     >
       {children}
     </div>
