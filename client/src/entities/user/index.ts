cb883bca3523246311626d4f3601cd390e5c20fb
Eugene Taranov <tayev021@gmail.com>
2026-05-27 22:03:26 +0300

    feat(client): add UserAvatar ui in user entity

--- client/src/entities/user/index.ts
+++ client/src/entities/user/index.ts
@@ -1,6 +1,7 @@
 import { useMe } from './hooks/useMe';
 import { NoUserAvatar } from './ui/NoUserAvatar';
+import { UserAvatar } from './ui/UserAvatar';
 import { UserAvatarLink } from './ui/UserAvatarLink';
 import { UserLink } from './ui/UserLink';
 
-export { useMe, NoUserAvatar, UserAvatarLink, UserLink };
+export { useMe, NoUserAvatar, UserAvatar, UserAvatarLink, UserLink };
