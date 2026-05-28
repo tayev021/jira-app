cf9e59a7704dd958f3631fb1ccb5e4b7caa5103e
Eugene Taranov <tayev021@gmail.com>
2026-05-28 20:12:10 +0300

    feat(client): add profile page

---
+++ client/src/pages/profile/ui/ProfilePage.tsx
@@ -0,0 +1,10 @@
+import { Container } from '../../../shared/ui/Container';
+import { Profile } from '../../../widgets/Profile';
+
+export function ProfilePage() {
+  return (
+    <Container className="pt-10">
+      <Profile />
+    </Container>
+  );
+}
