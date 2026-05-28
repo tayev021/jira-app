66ffb906492674cbea476b4e16987cae2485fe41
Eugene Taranov <tayev021@gmail.com>
2026-05-28 19:02:11 +0300

    feat(client): add account/settings page

---
+++ client/src/pages/account/index.ts
@@ -0,0 +1,5 @@
+import { AccountBioPage } from './bio/AccountBioPage';
+import { AccountIssuesPage } from './issues/AccountIssuesPage';
+import { AccountSettingsPage } from './settings/AccountSettingsPage';
+
+export { AccountBioPage, AccountIssuesPage, AccountSettingsPage };
---
+++ client/src/pages/account/settings/AccountSettingsPage.tsx
@@ -0,0 +1,5 @@
+import { AccountSettings } from '../../../widgets/AccountSettings/ui/AccountSettings';
+
+export function AccountSettingsPage() {
+  return <AccountSettings />;
+}
