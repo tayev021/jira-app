804773d9b361fb6b212e90b9dd15960b66f39d68
Eugene Taranov <tayev021@gmail.com>
2026-05-28 20:08:13 +0300

    feat(server): update UserRouter

--- server/src/modules/user/user.router.ts
+++ server/src/modules/user/user.router.ts
@@ -10,6 +10,7 @@ import { validateBody } from '../../shared/middleware/validateBody';
 const userRouter = Router();
 
 userRouter.get('/', protect, userController.getUsers);
+userRouter.get('/:userId', protect, userController.getUser);
 userRouter.get(
   '/search',
   protect,
