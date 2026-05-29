424f86a97860879f162a5752972b1e5a8a2fc57e
Eugene Taranov <tayev021@gmail.com>
2026-05-29 22:19:52 +0300

    feat(client): update SignInPage

--- client/src/pages/auth/signin/SignInPage.tsx
+++ client/src/pages/auth/signin/SignInPage.tsx
@@ -1,6 +1,14 @@
+import { useAuth } from '../../../shared/hooks/useAuth';
+import { Navigate } from 'react-router';
 import { SignInForm } from '../../../features/signIn';
 
 export function SignInPage() {
+  const { currentUser } = useAuth();
+
+  if (currentUser) {
+    return <Navigate to="/app" replace />;
+  }
+
   return (
     <div className="h-full p-5 pt-28 grid grid-cols-[minmax(min-content,1fr)_1fr] gap-5 bg-bg-secondary">
       <div>
