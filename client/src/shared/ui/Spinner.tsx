1f2229d8f8e1c0373b3c34e7e1ef2f2aa741cfb4
Eugene Taranov <tayev021@gmail.com>
2026-05-31 21:03:27 +0300

    feat(client): add shared Spinner ui

---
+++ client/src/shared/ui/Spinner.tsx
@@ -0,0 +1,5 @@
+export function Spinner() {
+  return (
+    <div className="w-14 h-14 border-4 border-t-primary border-r-transparent border-b-primary border-l-transparent rounded-full animate-spin"></div>
+  );
+}
