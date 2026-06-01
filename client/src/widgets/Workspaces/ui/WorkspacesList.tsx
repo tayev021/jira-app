66cd29465ec58828a20614772bfcc5feb0c3c299
Eugene Taranov <tayev021@gmail.com>
2026-06-01 10:33:02 +0300

    feat(client): update Workspaces widget

--- client/src/widgets/Workspaces/ui/WorkspacesList.tsx
+++ client/src/widgets/Workspaces/ui/WorkspacesList.tsx
@@ -1,14 +1,21 @@
 import { useWorkspaces } from '../../../entities/workspace';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CreateWorkspace } from '../../../features/createWorkspace/insex';
 import { CreateWorkspaceButton } from './CreateWorkspaceButton';
 import { NoWorkspaces } from './NoWorkspaces';
 import { WorkspaceItem } from './WorkspaceItem';
 import { Loader } from './Loader/Loader';
+import toast from 'react-hot-toast';
 
 export function WorkspacesList() {
-  const { workspaces = [], isLoading } = useWorkspaces();
+  const { workspaces = [], isLoading, isError, error } = useWorkspaces();
   const [isCreating, setIsCreating] = useState(false);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error!.message);
+    }
+  }, [isError, error]);
 
   if (isLoading) return <Loader />;
 
