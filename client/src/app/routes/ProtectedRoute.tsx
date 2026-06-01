35f840d47eb97e33d3c50c8512c449149c74d9af
Eugene Taranov <tayev021@gmail.com>
2026-06-01 10:34:14 +0300

    feat(client): update ProtectedRoute

--- client/src/app/routes/ProtectedRoute.tsx
+++ client/src/app/routes/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from 'react-router';
 import { useAuth } from '../../shared/hooks/useAuth';
+import { AppLoader } from '../../shared/ui/AppLoader';
 
 export function ProtectedRoute() {
   const { currentUser, isLoading } = useAuth();
 
-  if (isLoading) return 'Loader placeholder';
+  if (isLoading) return <AppLoader />;
 
   if (!currentUser) {
     return <Navigate to="/auth/signin" replace />;
