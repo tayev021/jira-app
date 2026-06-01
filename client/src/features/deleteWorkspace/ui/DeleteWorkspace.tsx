a57586bdb102c97e72bb12fbb7b018ea2ba205c4
Eugene Taranov <tayev021@gmail.com>
2026-06-01 10:23:44 +0300

    feat(client): update deleteWorkspace feature

--- client/src/features/deleteWorkspace/ui/DeleteWorkspace.tsx
+++ client/src/features/deleteWorkspace/ui/DeleteWorkspace.tsx
@@ -1,21 +1,19 @@
-import { useWorkspace } from '../../../entities/workspace';
 import { useDeleteWorkspace } from '../hooks/useDeleteWorkspace';
 import { Button } from '../../../shared/ui/Button';
 import toast from 'react-hot-toast';
 
 interface DeleteWorkspaceProps {
+  workspaceName: string;
   close?: () => void;
   handleSuccess?: () => void;
 }
 
 export function DeleteWorkspace({
+  workspaceName,
   close = () => {},
   handleSuccess = () => {},
 }: DeleteWorkspaceProps) {
-  const { workspace } = useWorkspace();
   const mutation = useDeleteWorkspace();
-
-  if (!workspace) return null;
 
   const handleDelete = () => {
     mutation.mutate(null, {
@@ -33,7 +31,7 @@ export function DeleteWorkspace({
     <div className="flex flex-col gap-3 px-10 py-6 border border-gray-primary rounded-md bg-primary-bg shadow-md leading-none">
       <h5 className="font-medium text-base text-center">
         Delete workspace{' '}
-        <span className="font-semibold text-primary">{workspace.name}</span>
+        <span className="font-semibold text-primary">{workspaceName}</span>
       </h5>
       <p>Are you sure you want to delete this workspace?</p>
       <div className="flex justify-center gap-10 mt-5">
