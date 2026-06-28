1059b6efce6a4e0e225d38d430ae81ddff0b6621
Eugene Taranov <tayev021@gmail.com>
2026-06-28 21:26:33 +0300

    build(client): update configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -7,5 +8,24 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: Number(process.env.CLIENT_PORT) || 3000,
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/shared/test/setup.ts'],
+    globals: true,
+    bail: 1,
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      exclude: [
+        'src/**/index.ts',
+        'src/**/*.test.ts',
+        'src/**/*.test.tsx',
+        'src/**/*.schema.ts',
+        'src/main.tsx',
+        'src/shared/test/**',
+        'src/shared/types/**',
+      ],
+    },
   },
 });
