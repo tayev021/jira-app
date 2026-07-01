37b56ee8bc4a9918a77dd017dff0116103088164
Eugene Taranov <tayev021@gmail.com>
2026-07-01 07:08:19 +0300

    fix(client): fix NoWorkspaces ui

--- client/src/widgets/Workspaces/ui/NoWorkspaces.tsx
+++ client/src/widgets/Workspaces/ui/NoWorkspaces.tsx
@@ -1,6 +1,7 @@
 export function NoWorkspaces() {
   return (
     <li className="flex gap-3 px-3 py-2 justify-center items-center border rounded-xl border-gray-primary-light text-base text-gray-primary italic">
+      <span className="h-10"></span>
       No workspaces yet
     </li>
   );
