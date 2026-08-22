91a15681400496ddfd6f1db7a45c0e08bb2c6c0b
Eugene Taranov <tayev021@gmail.com>
2026-08-22 14:42:56 +0300

    Revert "chore: test CI workflow with failed test"
    This reverts commit 5cfffbe499a9f3bd386f3611a0a4763892736b53.

--- server/src/tests/temp.test.ts
+++
@@ -1,9 +0,0 @@
-describe('FAILED TEST for check CI workflow', () => {
-  it('FAILED TEST', async () => {
-    function helloCI() {
-      return 'Hello CI workflow';
-    }
-
-    expect(helloCI()).toBe('');
-  });
-});
